aabeef1223373db63e91feda7b205451c229d1b8
thesturs <thesturs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-14 19:25:41 +0000

    one1
    git-svn-id: https://plugins.svn.wordpress.org/i-agree/trunk@697419 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ i-agree.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: I-Agree
+Plugin URI: http://wordpress.org/extend/plugins/
+Description: Force your users to agree to your terms when they login.
+Version: 0.1.0
+Author: Michael Stursberg
+Author URI: 
+*/
+
+
+
+// As part of WP authentication process, call our function
+add_filter('wp_authenticate_user', 'wp_authenticate_user_acc', 99999, 2);
+
+function wp_authenticate_user_acc($user, $password) {
+	 $dbfail = get_option('ia_fail');
+	      // See if the checkbox #login_accept was checked
+    if ( isset( $_REQUEST['login_accept'] ) && $_REQUEST['login_accept'] == 'on' ) {
+        // Checkbox on, allow login
+        return $user;
+    } else {
+        // Did NOT check the box, do not allow login
+        $error = new WP_Error();
+        $error->add('did_not_accept', $dbfail );
+        return $error;
+    }
+}
+
+// As part of WP login form construction, call our function
+add_filter ( 'login_form', 'login_form_acc' );
+
+function login_form_acc(){
+	$dbtermm = get_option('ia_termm');
+
+    // Add an element to the login form, which must be checked
+    echo '<br><label><input type="checkbox" name="login_accept" id="login_accept" />&nbsp;'.$dbtermm.'</label></br></br>';
+}
+
+add_action('admin_menu', 'i_agree_options');
+function i_agree_options() {
+  
+  add_options_page('i_agree', 'I Agree (Options)', 'manage_options', 'my-unique-identifier2', 'iaoptions');
+ 
+}
+
+		function iaoptions() {
+		  
+			include('ia-options.php');
+		}
---
+++ ia-options.php
@@ -0,0 +1,56 @@
+<?php 
+	    if($_POST['ia_hidden'] == 'Y') {
+			//Form data sent
+			$dbfail = $_POST['ia_fail'];
+			update_option('ia_fail', $dbfail);
+		  
+		  $dbtermm = $_POST['ia_termm'];
+			update_option('ia_termm', $dbtermm);
+	
+			 ?> 
+		
+			<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
+			<?php
+		} else {
+			//Normal page display
+			$dbfail = get_option('ia_fail');
+		  $dbtermm = get_option('ia_termm');
+		 
+			
+		}
+	?>
+	
+<style>
+.mes {
+color: red;
+}
+form {
+width: 725px;
+}
+textarea {
+clear: right;
+float: right;
+}
+input[type="submit"] {
+margin-top: 117px;
+}
+</style>
+<div class="wrap">
+
+			<?php    echo "<h2>" . __( 'I Agree', 'ai_trdom' ) . "</h2>"; ?>
+			
+			<form name="ia_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+				<input type="hidden" name="ia_hidden" value="Y">
+				<?php    echo "<h4>" . __( 'Settings', 'ia_trdom' ) . "</h4>"; ?>
+				<p><?php _e("Failed to agree error message: " ); ?><input type="text" name="ia_fail" value="<?php echo $dbfail; ?>" size="20"></br><?php _e("<span class='mes'>This is the text that shows when the visitor fails to click the check box</span>" ); ?></p>
+			
+				<p><?php _e("Message: " ); ?><textarea  name="ia_termm" cols="80" rows="10" ><?php echo $dbtermm; ?></textarea></br><?php _e("<span class='mes'>This the text that goes right after the checkbox</span>" ); ?></p>
+
+			
+				<p class="submit">
+				<input type="submit" name="Submit" value="<?php _e('Update Options', 'ia_trdom' ) ?>" />
+				</p>
+			</form>
+
+		</div>
+	
